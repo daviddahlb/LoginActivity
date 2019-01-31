6dbef4813311feeaa864f52b0716bde4cd8f98a7
daviddahlb <davidsdahlberg@gmail.com>
2019-01-30 23:22:29 -0800

    Added Toast to sign-in button

--- app/src/main/java/com/dsd3/loginactivity/MainActivity.java
+++ app/src/main/java/com/dsd3/loginactivity/MainActivity.java
@@ -2,6 +2,9 @@ package com.dsd3.loginactivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginButton = findViewById(R.id.btnLogin);
+
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this, "Sign in Button pressed...", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
